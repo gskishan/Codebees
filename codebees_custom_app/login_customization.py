212f5c57f2653158198b9a2b61928d5ec0e1bf65
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-23 11:01:35 +0530

    Update login_customization.py
    debigging

--- codebees_custom_app/login_customization.py
+++ codebees_custom_app/login_customization.py
@@ -16,6 +16,7 @@ def successful_login():
 def should_force_password_reset(user_id):
     sql="""select count(user) ct from `tabSessions` where user="{0}" """.format(user_id)
     data=frappe.db.sql(sql,as_dict=1)
+    frappe.errprint(data)
     if not data:
         frappe.errprint("data")
         return True
