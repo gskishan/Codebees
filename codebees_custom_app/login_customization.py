dfdf6f158737fec540eda215efbfd85b9ca8b07b
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-23 15:40:01 +0530

    Update login_customization.py

--- codebees_custom_app/login_customization.py
+++ codebees_custom_app/login_customization.py
@@ -5,21 +5,10 @@ from frappe import _, msgprint, throw
 @frappe.whitelist(allow_guest=True)
 def successful_login():
     user_id =  frappe.get_value('User', frappe.session.user, 'email')
+    should_force_password_reset =  frappe.get_value('User',user_id, 'last_login')
 
     # Check if the user needs to reset their password
-    if should_force_password_reset(user_id):
+    if should_force_password_reset:
         message = 'Click <a href="/update-password">Reset Your PassWord</a>'
         frappe.throw(message)
 
-        # frappe.throw(frappe.AuthenticationError)
-
-def should_force_password_reset(user_id):
-    sql="""select count(user) ct from `tabSessions` where user="{0}" """.format(user_id)
-    data=frappe.db.sql(sql,as_dict=1)
-    if not data:
-        return True
-    else:
-        if data[0].ct ==0 or  data[0].ct ==1:
-            return True
-        else:
-            return False
