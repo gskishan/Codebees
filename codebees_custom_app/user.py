ca8b0e0e42bfb2989c7a0e81fa41d65dbc846c0c
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-23 16:31:33 +0530

    Update user.py

--- codebees_custom_app/user.py
+++ codebees_custom_app/user.py
@@ -1,3 +1,6 @@
 import frappe
 def on_update(self,method):
-   self.db_set("is_pass",1)
+   if not self.last_login and frappe.db.exists("User Name", self.name):
+      doc=frappe.new_doc("User Name")
+      doc.user_name=self.name
+      doc.save()
