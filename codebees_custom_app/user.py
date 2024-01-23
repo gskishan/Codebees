b30a04ea91ba68db32fd8079b6463e969b54ff1f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-23 16:41:58 +0530

    Update user.py

--- codebees_custom_app/user.py
+++ codebees_custom_app/user.py
@@ -1,6 +1,3 @@
 import frappe
-def on_update(self,method):
-   if not self.last_login and frappe.db.exists("User Name", self.name):
-      doc=frappe.new_doc("User Name")
-      doc.user_name=self.name
-      doc.save()
+def validate(self,method):
+   self.simultaneous_sessions=5
