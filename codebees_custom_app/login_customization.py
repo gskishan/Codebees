06aae33385e66f4e37bad5f1c8acdf9caa37f774
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-23 10:52:54 +0530

    Update login_customization.py

--- codebees_custom_app/login_customization.py
+++ codebees_custom_app/login_customization.py
@@ -19,7 +19,7 @@ def should_force_password_reset(user_id):
     if not data:
         return True
     else:
-        if data[0].ct >1:
+        if data[0].ct <1:
             return True
         else:
             return False
