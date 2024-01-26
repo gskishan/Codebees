d2520953cdca4cd6f267ab6b40b47652c0b995c3
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-26 12:15:17 +0530

    Update hooks.py

--- codebees_custom_app/hooks.py
+++ codebees_custom_app/hooks.py
@@ -33,7 +33,7 @@ app_license = "mit"
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-doctype_js = {"Employee":"custom_script/employee/employee.js",
+doctype_js = {"Employee":"custom_script/employee/employee.js"
 }
 # Home Pages
 # ----------
