dfb4811c0ad1090aba2c59e36fd68b0a5dd9bac2
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-26 11:05:27 +0530

    Update hooks.py

--- codebees_custom_app/hooks.py
+++ codebees_custom_app/hooks.py
@@ -33,7 +33,8 @@ app_license = "mit"
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
+doctype_js = {"Employee":"custom_script/employee/employee.js",
+}
 # Home Pages
 # ----------
 on_session_creation  = 'codebees_custom_app.login_customization.successful_login'
