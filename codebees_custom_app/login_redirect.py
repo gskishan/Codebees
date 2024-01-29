676aeae9803125eb8a67d37e69e26cae272118ee
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-01-29 22:35:21 +0530

    Delete codebees_custom_app/login_redirect.py

--- codebees_custom_app/login_redirect.py
+++
@@ -1,8 +0,0 @@
-# In your custom Python code
-import frappe
-
-@frappe.whitelist()
-def your_custom_method_to_get_employee_profile_link(user):
-    # Assuming you want to redirect employees to their profile page
-    employee_profile_link = f'/app/employee/{user}'
-    return {'redirect_url': employee_profile_link}
