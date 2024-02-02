5207aa0015d51790160bd1489978acd8c2b1e761
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-02 09:52:17 +0530

    Update Employee.js

--- codebees_custom_app/Employee.js
+++ codebees_custom_app/Employee.js
@@ -1,56 +1,60 @@
 frappe.ui.form.on('Employee', {
     refresh: function(frm) {
-        // Get the current user's roles
+        
         var userRoles = frappe.user_roles;
+        
 
         // Check if the user has the "System Manager" role
-        var isSystemManager = userRoles.includes("System Manager","HR User","HR Manager");
+        var isSystemManager = userRoles.includes("System Manager");
+        var isHRUser = userRoles.includes("HR User");
+        var isHRManager = userRoles.includes("HR Manager");
 
         // Make fields read-only based on the user's role
-        frm.set_df_property('date_of_joining', 'read_only', !isSystemManager);
-        frm.set_df_property('status', 'read_only', !isSystemManager);
-        frm.set_df_property('user_id', 'read_only', !isSystemManager);
-        frm.set_df_property('create_user', 'read_only', !isSystemManager);
-        frm.set_df_property('create_user_permission', 'read_only', !isSystemManager);
-        frm.set_df_property('company', 'read_only', !isSystemManager);
-        frm.set_df_property('employee_number', 'read_only', !isSystemManager);
-        frm.set_df_property('designation', 'read_only', !isSystemManager);
-        frm.set_df_property('reports_to', 'read_only', !isSystemManager);
-        frm.set_df_property('custom_report_manager', 'read_only', !isSystemManager);
-        frm.set_df_property('department', 'read_only', !isSystemManager);
-        frm.set_df_property('employment_type', 'read_only', !isSystemManager);
-        frm.set_df_property('employment_details', 'read_only', !isSystemManager);
-        frm.set_df_property('company_email', 'read_only', !isSystemManager);
-        frm.set_df_property('attendance_device_id', 'read_only', !isSystemManager);
-        frm.set_df_property('holiday_list', 'read_only', !isSystemManager);
-        frm.set_df_property('approvers_section', 'read_only', !isSystemManager);
-        frm.set_df_property('default_shift', 'read_only', !isSystemManager);
-        frm.set_df_property('expense_approver', 'read_only', !isSystemManager);
-        frm.set_df_property('shift_request_approver', 'read_only', !isSystemManager);
-        frm.set_df_property('leave_approver', 'read_only', !isSystemManager);
-        frm.set_df_property('ctc', 'read_only', !isSystemManager);
-        frm.set_df_property('salary_currency', 'read_only', !isSystemManager);
-        frm.set_df_property('salary_mode', 'read_only', !isSystemManager);
-        frm.set_df_property('payroll_cost_center', 'read_only', !isSystemManager);
-        frm.set_df_property('pan_number', 'read_only', !isSystemManager);
-        frm.set_df_property('custom_esi_number', 'read_only', !isSystemManager);
-        frm.set_df_property('bank_name', 'read_only', !isSystemManager);
-        frm.set_df_property('bank_ac_no', 'read_only', !isSystemManager);
-        frm.set_df_property('ifsc_code', 'read_only', !isSystemManager);
-        frm.set_df_property('branch', 'read_only', !isSystemManager);
-        frm.set_df_property('grade', 'read_only', !isSystemManager);
-        frm.set_df_property('employee_name', 'read_only', !isSystemManager);
-        frm.set_df_property('first_name', 'read_only', !isSystemManager);
-        frm.set_df_property('last_name', 'read_only', !isSystemManager);
-        frm.set_df_property('middle_name', 'read_only', !isSystemManager);
-        frm.set_df_property('date_of_birth', 'read_only', !isSystemManager);
-        frm.set_df_property('job_applicant', 'read_only', !isSystemManager);
-        frm.set_df_property('final_confirmation_date', 'read_only', !isSystemManager);
-        frm.set_df_property('notice_number_of_days', 'read_only', !isSystemManager);
-        frm.set_df_property('scheduled_confirmation_date', 'read_only', !isSystemManager);
-        frm.set_df_property('contract_end_date', 'read_only', !isSystemManager);
-        frm.set_df_property('date_of_retirement', 'read_only', !isSystemManager);
-        frm.set_df_property('prefered_contact_email', 'read_only', !isSystemManager);
-        frm.set_df_property('provident_fund_account', 'read_only', !isSystemManager);
+        // List of fields to be made read-only
+        var readOnlyFields = [
+            'date_of_joining', 'status', 'user_id', 'create_user', 'create_user_permission',
+            'company', 'employee_number', 'designation', 'reports_to', 'custom_report_manager',
+            'department', 'employment_type', 'employment_details', 'company_email',
+            'attendance_device_id', 'holiday_list', 'approvers_section', 'default_shift',
+            'expense_approver', 'shift_request_approver', 'leave_approver', 'ctc',
+            'salary_currency', 'salary_mode', 'payroll_cost_center', 'pan_number',
+            'custom_esi_number', 'bank_name', 'bank_ac_no', 'ifsc_code', 'branch',
+            'grade', 'employee_name', 'first_name', 'last_name', 'middle_name',
+            'date_of_birth', 'job_applicant', 'final_confirmation_date',
+            'notice_number_of_days', 'scheduled_confirmation_date', 'contract_end_date',
+            'date_of_retirement', 'prefered_contact_email', 'provident_fund_account',
+            'resignation_letter_date', 'relieving_date', 'held_on', 'leave_encashed',
+            'new_workplace', 'encashment_date', 'reason_for_leaving', 'feedback', 'lft',
+            'rgt', 'old_parent'
+        ];
+        
+        // Set read-only properties for specified fields based on user roles
+        for (var field of readOnlyFields) {
+            frm.set_df_property(field, 'read_only', !(isSystemManager || isHRUser || isHRManager));
+        }
+
+
+        if (!isHRManager && !isSystemManager) {
+            //$("#employee-exit-tab").css("display", "none");
+            $("#employee-salary_information-tab").css("display", "none");
+            $("#employee-connections_tab-tab").css("display","none")
+        }
+
+        if (!isHRManager && !isSystemManager && !isHRUser) {
+            $("#employee-profile_tab-tab").css("display", "none");
+        }
+        if (!isSystemManager && !isHRManager) {
+            // Hide specific sidebar menus
+            ["form-assignments", "form-attachments", "form-tags", "form-shared", "form-sidebar-stats","form-sidebar-stats","sidebar-menu.text-muted"].forEach(function(menu) {
+                $("ul.list-unstyled.sidebar-menu." + menu).attr("hidden", true);
+            });
+            // Hide the new-timeline section
+            frm.page.wrapper.find(".new-timeline").css({'display':'none'});
+            
+        if (!isSystemManager){
+            frm.dashboard.hide();
+        }
+        }
     }
+    
 });
