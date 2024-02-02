4c3d519b5b2b78c0cb0c46e06fb9dcef54bb3c6d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-02 10:41:46 +0530

    Create attendence.js

---
+++ codebees_custom_app/custom_script/Attendence/attendence.js
@@ -0,0 +1,20 @@
+frappe.ui.form.on('Attendance Request', {
+	refresh(frm) {
+		var userRoles = frappe.user_roles;
+        
+
+        // Check if the user has the "System Manager" role
+        var isSystemManager = userRoles.includes("System Manager");
+        var isHRUser = userRoles.includes("HR User");
+        var isHRManager = userRoles.includes("HR Manager");
+	    
+	if (!isSystemManager && !isHRManager) {
+            // Hide specific sidebar menus
+            ["form-assignments", "form-attachments", "form-tags", "form-shared", "form-sidebar-stats","form-sidebar-stats","sidebar-menu.text-muted"].forEach(function(menu) {
+                $("ul.list-unstyled.sidebar-menu." + menu).attr("hidden", true);
+            });
+            // Hide the new-timeline section
+            frm.page.wrapper.find(".new-timeline").css({'display':'none'});
+	}
+	}
+})
