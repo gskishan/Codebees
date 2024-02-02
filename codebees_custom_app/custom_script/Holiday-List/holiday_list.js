06e0bf09dbbd2a6797d680440ae92669ed06f7af
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-02 10:41:10 +0530

    Create holiday_list.js

---
+++ codebees_custom_app/custom_script/Holiday-List/holiday_list.js
@@ -0,0 +1,30 @@
+frappe.ui.form.on('Holiday List', {
+    refresh: function(frm) {
+        // Get the user's roles
+        var userRoles = frappe.user_roles;
+
+        // Check if the user has the "System Manager" or "HR Manager" role
+        var isSystemManager = userRoles.includes("System Manager");
+        var isHRManager = userRoles.includes("HR Manager");
+
+        if (!isSystemManager && !isHRManager) {
+            // Hide specific sidebar menus
+            ["form-assignments", "form-attachments", "form-tags", "form-shared", "form-sidebar-stats","form-sidebar-stats","sidebar-menu"].forEach(function(menu) {
+                $("ul.list-unstyled.sidebar-menu." + menu).attr("hidden", true);
+            });
+
+            // Hide the new-timeline section
+            frm.page.wrapper.find(".new-timeline").css({'display':'none'});
+        }
+        if (!isSystemManager){
+            frm.dashboard.hide();
+        }
+        
+        
+        // Show or hide the 'add_weekly_holidays' section based on the user's role
+        frm.toggle_display('add_weekly_holidays', isSystemManager);
+
+        // Hide or show the entire button section based on the user's role
+        frm.fields_dict['clear_table'].wrapper.toggle(isSystemManager);
+    }
+});
