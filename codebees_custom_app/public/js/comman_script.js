9449380b3cf91710a7a3fddba06fcbd9abeccdfd
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-10 12:35:40 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,7 +1,1 @@
-frappe.ui.form.on('Form', {
-    refresh: function(frm) {
-     
-        console.log('Common Script Refreshed for DocType: ' + frm.doctype);
-    }
-
-});
+console.log("yes","wormg")
