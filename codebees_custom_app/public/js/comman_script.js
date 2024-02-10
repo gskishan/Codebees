44274be14b2cb7071875b7844abe5dc6a6093a81
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-10 13:44:54 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,4 +1,13 @@
-
+setTimeout(function(){
+    $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+    var userRoles = frappe.user_roles;     
+    var Workspace = userRoles.includes("Workspace Manager");
+    if (!Workspace){
+        $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
+        $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
+    }
+    console.log("out")
+}, 1000);
 
 function includeCustomJS() {
 setTimeout(function(){
@@ -9,6 +18,7 @@ setTimeout(function(){
         $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
         $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
     }
+     console.log("in")
 }, 1000);
 }
 
