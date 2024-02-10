7b4f9e6cb0064925a55c687eec9e3827fc4f379a
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-10 13:12:36 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,2 +1,10 @@
-console.log("yes","wormg")
- $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+setTimeout(function(){
+    $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+    var userRoles = frappe.user_roles;     
+    var Workspace = userRoles.includes("Workspace Manager");
+    if (!Workspace){
+        $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
+        $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
+    }
+}, 600);
+
