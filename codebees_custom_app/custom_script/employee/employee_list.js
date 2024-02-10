65b1390f498c51309fce8089189b9a81e99c8276
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-10 13:52:38 +0530

    Update employee_list.js

--- codebees_custom_app/custom_script/employee/employee_list.js
+++ codebees_custom_app/custom_script/employee/employee_list.js
@@ -1,21 +1,7 @@
 frappe.listview_settings['Employee'] = {
     refresh: function (listview) {
-        // Check if the current user has HR Manager or System Manager role
-        frappe.call({
-            method: "frappe.core.doctype.user.user.has_role",
-            args: {
-                roles: ["HR Manager", "System Manager"]
-            },
-            callback: function(r) {
-                if(r.message) {
-                    // User has one of the specified roles, keep the "List View" button visible
-                    console.log("working");
-                    $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").removeAttr("hidden");
-                } else {
-                    // User does not have the specified roles, hide the "List View" button
-                    $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
-                }
-            }
-        });
+      console.log("working")
+     $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+   
     }
-};
+   };
