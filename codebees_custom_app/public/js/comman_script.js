ae7e9101e595c0dd3519e234e9050a071b38c02c
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-10 14:59:50 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,18 +1,3 @@
-setTimeout(function(){
-    var userRoles = frappe.user_roles;     
-    var Workspace = userRoles.includes("Workspace Manager");
-    var hr = userRoles.includes("HR Manager");
-    var system = userRoles.includes("System Manager");
-    if (!Workspace){
-        $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
-        $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
-    }
-    if(!hr && !system){
-        $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
-    }
-    console.log("out")
-}, 1000);
-
 function includeCustomJS() {
     setTimeout(function(){
         var userRoles = frappe.user_roles;     
@@ -37,4 +22,14 @@ $(document).ready(function() {
 window.onpopstate = function() {
     includeCustomJS();
 };
+$(document).ready(function() {
+    $('.navbar').on('click', function(event) {
+        includeCustomJS();
+    });
+});
+
+document.addEventListener('click', function(event) {
+     includeCustomJS();
+});
+
 
