ab6e2f17b76d987b78ad2e27c0f274895040a949
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-10 14:03:26 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,25 +1,33 @@
 setTimeout(function(){
-    $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
     var userRoles = frappe.user_roles;     
     var Workspace = userRoles.includes("Workspace Manager");
+    var hr = userRoles.includes("HR Manager");
+    var system = userRoles.includes("System Manager");
     if (!Workspace){
         $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
         $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
+    }
+    if(!hr && !system){
+        $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
     }
     console.log("out")
 }, 1000);
 
 function includeCustomJS() {
-setTimeout(function(){
-    $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
-    var userRoles = frappe.user_roles;     
-    var Workspace = userRoles.includes("Workspace Manager");
-    if (!Workspace){
-        $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
-        $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
-    }
-     console.log("in")
-}, 1000);
+    setTimeout(function(){
+        var userRoles = frappe.user_roles;     
+        var Workspace = userRoles.includes("Workspace Manager");
+        var hr = userRoles.includes("HR Manager");
+        var system = userRoles.includes("System Manager");
+        if (!Workspace){
+            $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
+            $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
+        }
+        if(!hr && !system){
+            $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+        }
+        console.log("in")
+    }, 1000);
 }
 
 $(document).ready(function() {
