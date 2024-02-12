3c4cfac30ba6a99442656d96c38c5cd6b143cf58
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-12 11:28:05 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,18 +1,18 @@
 function includeCustomJS() {
-    var userRoles = frappe.user_roles;     
-    var isWorkspaceManager = userRoles.includes("Workspace Manager");
-    var isHRManager = userRoles.includes("HR Manager");
-    var isSystemManager = userRoles.includes("System Manager");
-
-    if (!isWorkspaceManager) {
-        $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
-        $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
-    }
-    
-    if (!isHRManager && !isSystemManager) {
-        $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
-    }
-    console.log("in");
+    setTimeout(function(){
+        var userRoles = frappe.user_roles;     
+        var Workspace = userRoles.includes("Workspace Manager");
+        var hr = userRoles.includes("HR Manager");
+        var system = userRoles.includes("System Manager");
+        if (!Workspace){
+            $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
+            $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
+        }
+        if(!hr && !system){
+            $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+        }
+        console.log("in")
+    }, 1000);
 }
 
 $(document).ready(function() {
@@ -22,7 +22,6 @@ $(document).ready(function() {
 window.onpopstate = function() {
     includeCustomJS();
 };
-
 $(document).ready(function() {
     $('.navbar').on('click', function(event) {
         includeCustomJS();
