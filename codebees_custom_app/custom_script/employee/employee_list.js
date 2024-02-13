06de2d434b5134d9dcc7eb0b9ab01be11aba5b85
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-13 16:18:41 +0530

    Update employee_list.js

--- codebees_custom_app/custom_script/employee/employee_list.js
+++ codebees_custom_app/custom_script/employee/employee_list.js
@@ -1,6 +1,6 @@
 frappe.listview_settings['Employee'] = {
     refresh: function (listview) {
-      console.log("working")
+      console.log("working1")
      $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
    
     }
