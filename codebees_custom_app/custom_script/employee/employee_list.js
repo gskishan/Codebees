26ba5ca30fad83678ebec2b9191b7773c54321b4
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-13 18:23:33 +0530

    Update employee_list.js

--- codebees_custom_app/custom_script/employee/employee_list.js
+++ codebees_custom_app/custom_script/employee/employee_list.js
@@ -1,7 +1,14 @@
 frappe.listview_settings['Employee'] = {
     refresh: function (listview) {
       console.log("working1")
-     $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+        var userRoles = frappe.user_roles;     
+        var hr = userRoles.includes("HR Manager");
+        var system = userRoles.includes("System Manager");
+        if(!hr && !system){
+            $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+        }
+
+
    
     }
    };
