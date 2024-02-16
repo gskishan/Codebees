383b02b43870b01e2a6a0250cbdf3632c3c7d6f7
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-16 15:49:55 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -12,6 +12,7 @@ function includeCustomJS() {
             $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
         }
         const url = window.location.href;
+        $('.dt-cell__edit input').prop('readonly', true);
 const containsReport = url.includes("view/report");
 
 if (containsReport) {
