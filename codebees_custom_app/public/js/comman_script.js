50120d44d7050321d4fb0b33ba1bcbfeed79d1ec
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-19 12:57:16 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,31 +1,26 @@
 function includeCustomJS() {
-    setTimeout(function() {
-        var userRoles = frappe.user_roles;
+    setTimeout(function(){
+        var userRoles = frappe.user_roles;     
         var Workspace = userRoles.includes("Workspace Manager");
         var hr = userRoles.includes("HR Manager");
         var system = userRoles.includes("System Manager");
-        if (!Workspace) {
+        if (!Workspace){
             $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
             $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
         }
-        if (!hr && !system) {
+        if(!hr && !system){
             $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
         }
-        console.log("in");
+
+        console.log("in")
     }, 1200);
 }
 
 function report_view() {
-    // Define a custom subclass
-    class CustomReportView extends frappe.views.ReportView {
-        is_editable(df, data) {
-            console.log("Working")
-            return false; // Always return false to prevent editing
-        }
-    }
-
-    // Replace ReportView with CustomReportView globally
-    frappe.views.ReportView = CustomReportView;
+    // Directly modify the is_editable function of frappe.views.ReportView
+    frappe.views.ReportView.prototype.is_editable = function(df, data) {
+        return false; // Always return false to prevent editing
+    };
 }
 
 $(document).ready(function() {
