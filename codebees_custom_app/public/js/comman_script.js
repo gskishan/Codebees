6819f34e404f8b4b1fdf190e40b500caba055c96
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-19 12:40:00 +0530

    Update comman_script.js

--- codebees_custom_app/public/js/comman_script.js
+++ codebees_custom_app/public/js/comman_script.js
@@ -1,49 +1,52 @@
 function includeCustomJS() {
-    setTimeout(function(){
-        var userRoles = frappe.user_roles;     
+    setTimeout(function() {
+        var userRoles = frappe.user_roles;
         var Workspace = userRoles.includes("Workspace Manager");
         var hr = userRoles.includes("HR Manager");
         var system = userRoles.includes("System Manager");
-        if (!Workspace){
+        if (!Workspace) {
             $(".custom-actions.hidden-xs.hidden-md button[data-label='Create%20Workspace']").attr("hidden", true);
             $(".custom-actions[data-label='Create Workspace'].hidden-xs.hidden-md, button.btn.btn-secondary.btn-default.btn-sm[data-label='Edit']").attr("hidden", true);
         }
-        if(!hr && !system){
+        if (!hr && !system) {
             $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
         }
-
-
-        console.log("in")
+        console.log("in");
     }, 1200);
-
-}
-function report_view(){
-frappe.views.ReportView.prototype.is_editable = function(df, data) {
-    return false;
-};
-
 }
 
+function report_view() {
+    // Define a custom subclass
+    class CustomReportView extends frappe.views.ReportView {
+        is_editable(df, data) {
+            console.log("Working")
+            return false; // Always return false to prevent editing
+        }
+    }
 
+    // Replace ReportView with CustomReportView globally
+    frappe.views.ReportView = CustomReportView;
 }
 
 $(document).ready(function() {
     includeCustomJS();
-    report_view()         
-});
+    report_view();
 
-window.onpopstate = function() {
-    includeCustomJS();
-    report_view()
-};
-$(document).ready(function() {
+    // Bind to window popstate event
+    window.onpopstate = function() {
+        includeCustomJS();
+        report_view();
+    };
+
+    // Bind to navbar click event
     $('.navbar').on('click', function(event) {
         includeCustomJS();
-        report_view()
+        report_view();
     });
-});
 
-document.addEventListener('click', function(event) {
-     includeCustomJS();
-    report_view()
+    // Bind to document click event
+    $(document).on('click', function(event) {
+        includeCustomJS();
+        report_view();
+    });
 });
