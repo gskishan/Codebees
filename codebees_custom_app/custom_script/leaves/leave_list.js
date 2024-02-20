f4eb65897ca0f8239543f47adfbf9b47cf4a9808
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-20 14:25:01 +0530

    Create leave_list.js

---
+++ codebees_custom_app/custom_script/leaves/leave_list.js
@@ -0,0 +1,14 @@
+frappe.listview_settings['Leave Application'] = {
+    refresh: function (listview) {
+      console.log("working1")
+        var userRoles = frappe.user_roles;     
+        var hr = userRoles.includes("HR Manager");
+        var system = userRoles.includes("System Manager");
+        if(!hr && !system){
+            $("span.custom-btn-group-label:contains('List View')").closest(".btn.ellipsis").attr("hidden", true);
+        }
+
+
+   
+    }
+   };
