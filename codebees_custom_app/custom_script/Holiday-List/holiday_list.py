38564f6384c6bec52b45cedda13bf675c3f5de80
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-23 23:48:07 +0530

    Create holiday_list.py

---
+++ codebees_custom_app/custom_script/Holiday-List/holiday_list.py
@@ -0,0 +1,11 @@
+import frappe
+from frappe import _
+
+
+@frappe.whitelist()
+def validate(self,method):
+  for d in self.holidays:
+    if not d.weekly_off:
+      hd=pur.append("custom_holidays",{})
+      hd.description=d.description
+      hd.holiday_date=d.holiday_date
